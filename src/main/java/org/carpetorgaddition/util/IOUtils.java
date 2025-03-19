f1a5c84362f188c8d0849f4f4d844b6f69f29087
fcsailboat <3286637847@qq.com>
2025-03-19 22:46:00 +0800

    固定开发环境玩家名

--- src/main/java/org/carpetorgaddition/util/IOUtils.java
+++ src/main/java/org/carpetorgaddition/util/IOUtils.java
@@ -17,8 +17,8 @@ public class IOUtils {
      */
     public static final String INVALID_FILENAME_CHARS = "\\/:*?\"<>|";
     public static final String[] WINDOWS_RESERVED_NAME = {
-            "CON", "PRN", "AUX",
-            "NUL", "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9",
+            "CON", "PRN", "AUX", "NUL",
+            "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9",
             "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9"
     };
 
