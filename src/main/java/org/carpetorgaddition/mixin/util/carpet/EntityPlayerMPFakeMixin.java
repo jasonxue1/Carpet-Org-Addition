dfd32abe8a814225563bbfbd4b98a45ecc437ee4
fcsailboat <3286637847@qq.com>
2025-06-24 02:54:40 +0800

    版本差异

--- src/main/java/org/carpetorgaddition/mixin/util/carpet/EntityPlayerMPFakeMixin.java
+++ src/main/java/org/carpetorgaddition/mixin/util/carpet/EntityPlayerMPFakeMixin.java
@@ -4,36 +4,39 @@ import carpet.patches.EntityPlayerMPFake;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.mojang.authlib.GameProfile;
 import org.carpetorgaddition.util.GenericUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 @Mixin(value = EntityPlayerMPFake.class, remap = false)
 public class EntityPlayerMPFakeMixin {
-    @WrapOperation(method = "createFake", at = @At(value = "INVOKE", target = "Ljava/util/concurrent/CompletableFuture;thenAcceptAsync(Ljava/util/function/Consumer;Ljava/util/concurrent/Executor;)Ljava/util/concurrent/CompletableFuture;"))
-    private static <T> CompletableFuture<Void> thenAcceptAsync(CompletableFuture<T> instance, Consumer<? super T> action, Executor executor, Operation<CompletableFuture<Void>> original) {
+    @WrapOperation(method = "createFake", at = @At(value = "INVOKE", target = "Ljava/util/concurrent/CompletableFuture;whenCompleteAsync(Ljava/util/function/BiConsumer;Ljava/util/concurrent/Executor;)Ljava/util/concurrent/CompletableFuture;"))
+    private static <T> CompletableFuture<T> whenCompleteAsync(CompletableFuture<Optional<GameProfile>> instance, BiConsumer<? super T, ? super Throwable> action, Executor executor, Operation<CompletableFuture<T>> original) {
         Consumer<EntityPlayerMPFake> onFakePlayerSpawning = GenericUtils.FAKE_PLAYER_SPAWNING.get();
         if (onFakePlayerSpawning == null) {
             return original.call(instance, action, executor);
         }
-        Consumer<? super T> consumer = value -> {
+        BiConsumer<? super T, Throwable> biConsumer = (value, throwable) -> {
             try {
                 GenericUtils.INTERNAL_FAKE_PLAYER_SPAWNING.set(onFakePlayerSpawning);
-                action.accept(value);
+                action.accept(value, throwable);
             } finally {
                 GenericUtils.INTERNAL_FAKE_PLAYER_SPAWNING.remove();
             }
         };
-        return original.call(instance, consumer, executor);
+        return original.call(instance, biConsumer, executor);
     }
 
-    @Inject(method = "lambda$createFake$2", at = @At("RETURN"))
+    @Inject(method = "lambda$createFake$2", at = @At(value = "INVOKE", target = "Lcarpet/patches/EntityPlayerMPFake;getAbilities()Lnet/minecraft/entity/player/PlayerAbilities;"))
     private static void spawn(CallbackInfo ci, @Local EntityPlayerMPFake fakePlayer) {
         Consumer<EntityPlayerMPFake> consumer = GenericUtils.INTERNAL_FAKE_PLAYER_SPAWNING.get();
         if (consumer == null) {
