523c5b433ef0e9373224db7fbf72bbebac3f46a0
fcsailboat <3286637847@qq.com>
2025-11-16 19:19:40 +0800

    版本差异

--- src/main/java/org/carpetorgaddition/mixin/rule/PlayerEntityMixin.java
+++ src/main/java/org/carpetorgaddition/mixin/rule/PlayerEntityMixin.java
@@ -33,6 +33,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
